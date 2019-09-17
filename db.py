523ec21d1be85b00fb3db89275f2fbc999d88023
Nick Morley <nick.morley111@gmail.com>
2019-09-17 11:20:36 +0100

    Add ability to specify db path different from that in config

--- db.py
+++ db.py
@@ -1,22 +1,33 @@
 import pickle
 from spacy.tokens import Doc
+from role_pattern_nlp.match import RolePatternMatch
 from db import sql
 import util
 
 
-def load_sentence_doc(sentence_id, vocab=None):
+def load_sentence_doc(sentence_id, vocab=None, **kwargs):
     query = 'select spacy_doc from spacy_sentence_doc where sentence_id = ?'
     values = (sentence_id,)
-    results = sql.db_query(query, values=values, fetch='one')
+    results = sql.db_query(query, values=values, fetch='one', **kwargs)
     spacy_doc_bytes = results[0]
     if not vocab:
         vocab = util.init_vocab()
     doc = Doc(vocab).from_bytes(spacy_doc_bytes)
+    query = 'select * from tokens where sentence_id = ?'
+    results = sql.db_query(query, values=values, **kwargs)
+    tokens = sql.rows_to_dicts(results, 'tokens', **kwargs)
+    tokens = [util.unpack_json_field(token, 'data') for token in tokens]
+    for token in tokens:
+        token_offset = token['token_offset']
+        spacy_token = doc[token_offset]
+        # Set custom extensions which are lost during seralisation
+        custom_features = token['features'].get('_')
+        util.set_token_extensions(spacy_token, custom_features)
     return doc
 
 
-def load_role_pattern(pattern_id):
-    row = sql.fetch_row('patterns', pattern_id, return_type='dict')
+def load_role_pattern(pattern_id, **kwargs):
+    row = sql.fetch_row('patterns', pattern_id, return_type='dict', **kwargs)
     if not row:
         raise Exception('No pattern found for id {}'.format(pattern_id))
     role_pattern_instance = row['role_pattern_instance']
@@ -24,31 +35,39 @@ def load_role_pattern(pattern_id):
     return role_pattern
 
 
-def despacify_match(match, sentence_id):
+def load_role_pattern_match(slots, match_tokens, sentence_id, **kwargs):
+    match = spacify_match(slots, sentence_id, **kwargs)
+    match_tokens = spacify_tokens(match_tokens, sentence_id, **kwargs)
+    match = RolePatternMatch(match)
+    match.match_tokens = match_tokens
+    return match
+
+
+def despacify_match(match, sentence_id, **kwargs):
     # Replace each token with its database representation
     slots = {}  # The labelled tokens
     for label, tokens in match.items():
         despacified_tokens = []
         for token in tokens:
-            despacified_token = token_from_db(sentence_id, token.i)
+            despacified_token = token_from_db(sentence_id, token.i, **kwargs)
             despacified_token = util.unpack_json_field(despacified_token, 'data')
             despacified_tokens.append(despacified_token)
         slots[label] = despacified_tokens
     match_tokens = match.match_tokens  # Includes the unlabelled tokens
-    match_tokens = [token_from_db(sentence_id, token.i) for token in match_tokens]
+    match_tokens = [token_from_db(sentence_id, token.i, **kwargs) for token in match_tokens]
     match_tokens = [util.unpack_json_field(token, 'data') for token in match_tokens]
     return slots, match_tokens
 
 
-def spacify_match(match, sentence_id):
+def spacify_match(match, sentence_id, **kwargs):
     for label, tokens in match.items():
-        spacy_tokens = spacify_tokens(tokens, sentence_id)
+        spacy_tokens = spacify_tokens(tokens, sentence_id, **kwargs)
         match[label] = spacy_tokens
     return match
 
 
-def spacify_tokens(tokens, sentence_id):
-    doc = load_sentence_doc(sentence_id)
+def spacify_tokens(tokens, sentence_id, **kwargs):
+    doc = load_sentence_doc(sentence_id, **kwargs)
     spacy_tokens = []
     for token in tokens:
         token_offset = token['token_offset']
@@ -60,10 +79,10 @@ def spacify_tokens(tokens, sentence_id):
     return spacy_tokens
 
 
-def token_from_db(sentence_id, token_offset):
+def token_from_db(sentence_id, token_offset, **kwargs):
     query = 'select * from tokens where sentence_id = {0} and token_offset = {1}'.format(
         sentence_id, token_offset
     )
-    token_row = sql.db_query(query, fetch='one')
-    token_row = sql.row_to_dict(token_row, 'tokens')
+    token_row = sql.db_query(query, fetch='one', **kwargs)
+    token_row = sql.row_to_dict(token_row, 'tokens', **kwargs)
     return token_row
--- sql.py
+++ sql.py
@@ -4,7 +4,7 @@ from config import config
 db_path = config['db_file_path']
 
 
-def db_query(query, values=None, fetch='all', return_id=False):
+def db_query(query, values=None, fetch='all', return_id=False, db_path=db_path):
     with sqlite3.connect(db_path) as con:
         cur = con.cursor()
         cur.execute('PRAGMA foreign_keys=ON')
@@ -23,24 +23,24 @@ def db_query(query, values=None, fetch='all', return_id=False):
     return result
 
 
-def fetch_row(table, row_id, return_type='tup'):
+def fetch_row(table, row_id, return_type='tup', **kwargs):
     query = 'select * from {0} where id = {1}'.format(table, row_id)
-    result = db_query(query, fetch='one')
+    result = db_query(query, fetch='one', **kwargs)
     if return_type == 'dict':
-        result = row_to_dict(result, table)
+        result = row_to_dict(result, table, **kwargs)
     return result
 
 
-def fetch_rows(table, row_ids, return_type='tup'):
+def fetch_rows(table, row_ids, return_type='tup', **kwargs):
     id_list = ', '.join([str(id_) for id_ in row_ids])
     query = 'select * from {0} where id IN ({1})'.format(table, id_list)
-    results = db_query(query, fetch='all')
+    results = db_query(query, fetch='all', **kwargs)
     if return_type == 'dict':
-        results = rows_to_dicts(results, table)
+        results = rows_to_dicts(results, table, **kwargs)
     return results
 
 
-def insert_row(table, data):
+def insert_row(table, data, **kwargs):
     column_names = []
     values = []
     for key, value in data.items():
@@ -56,27 +56,27 @@ def insert_row(table, data):
         placeholders,
     )
     values = tuple(values)
-    row_id = db_query(query, values=values, return_id=True)
+    row_id = db_query(query, values=values, return_id=True, **kwargs)
     return row_id
 
 
-def get_column_names(table):
+def get_column_names(table, **kwargs):
     query = 'PRAGMA TABLE_INFO({})'.format(table)
-    result = db_query(query, fetch='all')
+    result = db_query(query, fetch='all', **kwargs)
     names = [tup[1] for tup in result]
     return names
 
 
-def row_to_dict(row, table):
+def row_to_dict(row, table, **kwargs):
     if not row:
         return {}
-    column_names = get_column_names(table)
+    column_names = get_column_names(table, **kwargs)
     dict_ = dict(zip(column_names, row))
     return dict_
 
 
-def rows_to_dicts(rows, table):
-    column_names = get_column_names(table)
+def rows_to_dicts(rows, table, **kwargs):
+    column_names = get_column_names(table, **kwargs)
     dicts = []
     for row in rows:
         if not row:
@@ -87,8 +87,8 @@ def rows_to_dicts(rows, table):
     return dicts
 
 
-def get_ids(table):
+def get_ids(table, **kwargs):
     query = 'select id from {}'.format(table)
-    results = db_query(query)
+    results = db_query(query, **kwargs)
     ids = [result[0] for result in results]
     return ids
